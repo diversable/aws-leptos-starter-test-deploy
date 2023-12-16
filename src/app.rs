add4eb774e40f18573bb0b8cfa7b9f6f32856870
Diversable <dan.mantei@outlook.com>
2023-12-15 21:02:13 -0800

    update default project page

--- src/app.rs
+++ src/app.rs
@@ -43,7 +43,13 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <div>
+            <picture>
+                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
+                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+            </picture>
+            <h1>"Welcome to Leptos "<i>"On Shuttle.rs"</i>"!"</h1>
+            <button on:click=on_click>"Click Me: " {count}</button>
+        </div>
     }
 }
